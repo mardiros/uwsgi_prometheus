ffa182d0304bd663bc9d11d7bf749db687cf9e7d
Guillaume Gauvrit <guillaume@gauvr.it>
2018-09-29 15:23:22 +0200

    Expose a /metrics route

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ import logging
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
-from prometheus_client import Counter
+from prometheus_client import Counter, CollectorRegistry, generate_latest
+from prometheus_client.multiprocess import MultiProcessCollector
 
 log = logging.getLogger('app')
 
@@ -18,11 +19,21 @@ def hello_world(request):
     return Response('Hello World!')
 
 
+@view_config(route_name='metrics')
+def metrics(request):
+    log.info('Serving metrics')
+    registry = CollectorRegistry()
+    MultiProcessCollector(registry)
+    data = generate_latest(registry)
+    return Response(data, content_type='text/plain')
+
+
 with Configurator() as config:
     logging.basicConfig(level=logging.DEBUG)
     log.info('Starting')
     config.scan('.')
     config.add_route('hello', '/')
+    config.add_route('metrics', '/metrics')
 
     application = config.make_wsgi_app()
     log.info('Application built')
