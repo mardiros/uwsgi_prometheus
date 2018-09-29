8feb6187d7718648b1ab2075341e56dae7ebfe8f
Guillaume Gauvrit <guillaume@gauvr.it>
2018-09-29 15:23:03 +0200

    Add a prometheus counter

--- app/app.py
+++ app/app.py
@@ -4,13 +4,17 @@ import logging
 from pyramid.config import Configurator
 from pyramid.response import Response
 from pyramid.view import view_config
+from prometheus_client import Counter
 
 log = logging.getLogger('app')
+
+app_hello_count = Counter('app_hello_count', 'Number of hello world')
 
 
 @view_config(route_name='hello')
 def hello_world(request):
     log.info('Serving hello world')
+    app_hello_count.inc()
     return Response('Hello World!')
 
 
